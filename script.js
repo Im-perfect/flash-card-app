1804e6eab369c3494158ec68653dd2058f52f79d
Im-perfect <zhangke.kerri@gmail.com>
2019-09-19 16:46:18 +0200

    add another category backend, intialize the current flashcard category

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const flashCardContent = [
+const flashCardContent0 = [
     {
         question:'1. Où passes-tu tes vacances?',
         answer:'1. Where do you spend your holidays?'
@@ -20,6 +20,34 @@ const flashCardContent = [
         answer:'5. I go on holiday with ...'
     }
 ];
+const flashCardContent1 = [
+    {
+        question:'1. 你在哪里度假?',
+        answer:'1. Where do you spend your holidays?'
+    },
+    {
+        question:'2. 我在...度假',
+        answer:'2. I spend my holidays ...'
+    },
+    {
+        question:'3. 你去了多久?',
+        answer:'3. How long do you go for?'
+    }
+]
+const flashCardCategories = [
+    {
+        subject: 'French',
+        cards: flashCardContent0
+    },
+    {
+        subject: 'Chinese',
+        cards: flashCardContent1
+    },
+];
+
+//initialize current flash card category
+const flashCardContent = flashCardCategories[0].cards;
+
 document.getElementById('numberOfCards').innerHTML = flashCardContent.length;
 
 let currentIndexOfCards = 0; //Math.round(Math.random() * (flashCardContent.length - 1));
