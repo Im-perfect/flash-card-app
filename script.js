d965627754e62c1e1b26fc9e941671b3d3b6ef0e
Im-perfect <zhangke.kerri@gmail.com>
2019-09-20 14:33:36 +0200

    log history done

--- script.js
+++ script.js
@@ -2,59 +2,59 @@ const flashCardPool = [
   {
     question: "1. Où passes-tu tes vacances?",
     answer: "1. Where do you spend your holidays?",
-    category: 'French'
+    category: "French"
   },
   {
     question: "2. Je passe mes vacances ...",
     answer: "2. I spend my holidays ...",
-    category: 'French'
+    category: "French"
   },
   {
     question: "3. Combien de temps restes-tu en vacances?",
     answer: "3. How long do you go for?",
-    category: 'French'
+    category: "French"
   },
   {
     question: "4. Avec qui vas-tu en vacances?",
     answer: "4. Who do you go on holiday with?",
-    category: 'French'
+    category: "French"
   },
   {
     question: "5. Je vais en vacances avec ...",
     answer: "5. I go on holiday with ...",
-    category: 'French'
+    category: "French"
   },
   {
     question: "1. 你在哪里度假?",
     answer: "1. Where do you spend your holidays?",
-    category: 'Chinese'
+    category: "Chinese"
   },
   {
     question: "2. 我在...度假",
     answer: "2. I spend my holidays ...",
-    category: 'Chinese'
+    category: "Chinese"
   },
   {
     question: "3. 你去了多久?",
     answer: "3. How long do you go for?",
-    category: 'Chinese'
-  }    
+    category: "Chinese"
+  }
 ];
 
 //initialize current flash card category
-let flashCardContent = flashCardPool.filter((e)=>e.category === 'French');
+let flashCardContent = flashCardPool.filter(e => e.category === "French");
 let currentIndexOfCards = 0; //Math.round(Math.random() * (flashCardContent.length - 1));
-const previousHistory = null;
+const previousHistory = [];
 let previousIndexOfCards = null;
 let currentCard = flashCardContent[currentIndexOfCards];
 let currentCardContentElement = document.getElementById("flashCardContent");
 const currentCardElement = document.getElementById("flashCard");
-let language = 'French';
-
+let language = "French";
 
 //function to control which card to show, flip cards, previous, next cards
 const showCards = flashCardContent => {
-    console.log("showCards", flashCardContent);
+  console.log("showCards", flashCardContent);
+  console.log(previousHistory, previousHistory.length);
 
   let isQuestion = true;
 
@@ -91,9 +91,12 @@ const showCards = flashCardContent => {
   nextButton.addEventListener("click", () => {
     previousIndexOfCards = currentIndexOfCards;
     getRandomCard();
+    //if same card, do it again
     if (currentIndexOfCards === previousIndexOfCards) {
       getRandomCard();
     } else {
+      previousHistory.push(currentIndexOfCards);
+      console.log(previousHistory);
       currentCardContentElement.innerHTML =
         flashCardContent[currentIndexOfCards].question;
     }
@@ -101,10 +104,25 @@ const showCards = flashCardContent => {
 
   //click for the previous card, but only once...because the previous cards are all random, need to make an array to store all the previous card indexes.
   previousButton.addEventListener("click", () => {
-    if (previousIndexOfCards === null) {
+    // if (previousIndexOfCards === null) {
+    //   currentCardContentElement.innerHTML =
+    //     "<em>There is no previous card.</em>";
+    // } else {
+    //   currentCard = flashCardContent[previousIndexOfCards];
+    //   currentIndexOfCards = previousIndexOfCards;
+    //   currentCardContentElement.innerHTML =
+    //     flashCardContent[currentIndexOfCards].question;
+    // }
+    if (previousHistory.length === 1 || previousHistory.length === 0) {
       currentCardContentElement.innerHTML =
         "<em>There is no previous card.</em>";
+      previousHistory.pop();
     } else {
+      previousHistory.pop();
+      previousIndexOfCards = previousHistory.length - 1;
+
+      console.log(previousHistory, previousHistory.length);
+
       currentCard = flashCardContent[previousIndexOfCards];
       currentIndexOfCards = previousIndexOfCards;
       currentCardContentElement.innerHTML =
@@ -115,14 +133,13 @@ const showCards = flashCardContent => {
 
 //add a new card to the current category
 const submitNewCard = () => {
-    console.log('new card called');
+  console.log("new card called");
   const newQuestion = document.getElementById("newQuestion");
   const newAnswer = document.getElementById("newAnswer");
 
   if (!newQuestion.value.trim() || !newAnswer.value.trim()) {
     alert("Please fill in again.");
-  } 
-  else {
+  } else {
     pushCard(language);
 
     previousIndexOfCards = currentIndexOfCards;
@@ -139,43 +156,39 @@ const submitNewCard = () => {
 
 //show the total number of cards in the current category
 const totalOfCards = () => {
-    document.getElementById("numberOfCards").innerHTML = flashCardContent.length;
+  document.getElementById("numberOfCards").innerHTML = flashCardContent.length;
 };
 
 //add the new card to the card pool
-const pushCard = (language) => {
-    flashCardPool.push({
-        question: newQuestion.value,
-        answer: newAnswer.value,
-        category: language
-      });
-  
-      flashCardContent.push({
-          question: newQuestion.value,
-          answer: newAnswer.value,
-          category: language
-        });
-}
+const pushCard = language => {
+  flashCardPool.push({
+    question: newQuestion.value,
+    answer: newAnswer.value,
+    category: language
+  });
+
+  flashCardContent.push({
+    question: newQuestion.value,
+    answer: newAnswer.value,
+    category: language
+  });
+};
 
 showCards(flashCardContent);
 totalOfCards();
 
-document
-  .getElementById("French")
-  .addEventListener("click", () => {
-      language = 'French';
-      flashCardContent = flashCardPool.filter((e)=>e.category===language)
-      console.log("current cards", flashCardContent);
-      totalOfCards();
-      showCards(flashCardContent)
-    });
+document.getElementById("French").addEventListener("click", () => {
+  language = "French";
+  flashCardContent = flashCardPool.filter(e => e.category === language);
+  console.log("current cards", flashCardContent);
+  totalOfCards();
+  showCards(flashCardContent);
+});
 
-document
-  .getElementById("Chinese")
-  .addEventListener("click", () => {
-      language = 'Chinese';
-      flashCardContent = flashCardPool.filter((e)=>e.category === language);
-      console.log("current cards", flashCardContent);
-      totalOfCards();
-      showCards(flashCardContent);
-    });
+document.getElementById("Chinese").addEventListener("click", () => {
+  language = "Chinese";
+  flashCardContent = flashCardPool.filter(e => e.category === language);
+  console.log("current cards", flashCardContent);
+  totalOfCards();
+  showCards(flashCardContent);
+});
